ee604cd907f0f400662d26626f217adcba316e14
Ris Codes <riscodes@gmail.com>
2024-02-26 15:25:57 +0530

    Product Detail Page UI - Description

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:pixlit_store/common/widgets/texts/section_heading.dart';
+import 'package:pixlit_store/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
 import 'package:pixlit_store/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:pixlit_store/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:pixlit_store/ultils/constants/sizes.dart';
+import 'package:readmore/readmore.dart';
 
 import 'widgets/product_image_slider.dart';
 import 'widgets/rating_and_share.dart';
@@ -12,6 +16,7 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -41,9 +46,40 @@ class ProductDetail extends StatelessWidget {
                       width: double.infinity,
                       child: ElevatedButton(
                           onPressed: () {}, child: const Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwSections),
 
                   /// Description
+                  const TSectionHeading(
+                      title: 'Description', showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const ReadMoreText(
+                    'This is a Product description for Blue Nike Sleeve less vest. There are more things that can be added but i an just practicing and nothing else. ',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show  more',
+                    trimExpandedText: 'Less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
                   /// Reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                          title: 'Reviews (199)', showActionButton: false),
+                      IconButton(
+                        icon: const Icon(
+                            Iconsax.arrow_right_3, size: 18),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             ),
